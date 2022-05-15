630cc6cce56ebc58bf3839bb20ba94575684e326
Louise Willoughby <lou@Louises-MacBook-Air.local>
2022-05-15 14:38:50 +0100

    set up

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <MazeGame/>
+</template>
+
+<script>
+
+import MazeGame from './components/MazeGame.vue'
+
+export default {
+  name: "App",
+  components: {
+    MazeGame
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/MazeGame.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'MazeGame'
+}
+</script>
+
+<template>
+  <div class="hello">
+    Hello World
+  </div>
+</template>
+
+<style scoped>
+
+.hello {
+  color: aqua;
+}
+</style>
