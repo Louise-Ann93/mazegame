9bc926f545fcc42d8ac75386b42e13556058fea0
Louise Willoughby <lou@Louises-MacBook-Air.local>
2022-05-15 22:20:53 +0100

    cursor

--- src/components/MazeGame.vue
+++ src/components/MazeGame.vue
@@ -206,14 +206,14 @@ grid-template: repeat(6, 50px) / repeat(6, 50px);
 
 .wall {
   background-color: green;
-  cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='22' height='26' viewport='0 0 100 100' style='fill:black;font-size:13px;'><text y='50%'>💩</text></svg>") 16 0,auto; 
+  cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>❕</text></svg>") 16 0,auto;
 }
 .wall:hover {
   background-color: red;
 }
 .floor {
   background-color: lightgrey;
-  cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='22' height='26' viewport='0 0 100 100' style='fill:black;font-size:13px;'><text y='50%'>😀</text></svg>") 16 0,auto; 
+  cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>🦆</text></svg>") 16 0,auto;
 }
 
 .border-left {
